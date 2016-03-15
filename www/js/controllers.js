287afe9c3c22ba16f9def39515e00a7112ea716c
Jessica Hathaway <hathaway.jess@gmail.com>
2016-03-14 18:50:06 -0600

    startDate functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,80 +3,115 @@ angular.module('starter.controllers', [])
 var app = angular.module('starter.controllers');
 
 app.controller("calendarDemo", function($scope) {
-    $scope.day = moment();
+  $scope.day = moment();
 });
 
 app.directive("calendar", function() {
-    return {
-        restrict: "E",
-        templateUrl: "templates/calendar.html",
-        scope: {
-            selected: "="
-        },
-        link: function(scope) {
-            scope.selected = _removeTime(scope.selected || moment());
-            scope.month = scope.selected.clone();
+  return {
+    restrict: "E",
+    templateUrl: "templates/calendar.html",
+    scope: {
+      selected: "="
+    },
+    link: function(scope) {
+      scope.selected = _removeTime(scope.selected || moment());
+      scope.month = scope.selected.clone();
 
-            var start = scope.selected.clone();
-            start.date(1);
-            _removeTime(start.day(0));
 
-            _buildMonth(scope, start, scope.month);
+      var start = scope.selected.clone();
+      start.date(1);
+      _removeTime(start.day(0));
 
-            scope.select = function(day) {
-                scope.selected = day.date;
-            };
+      _buildMonth(scope, start, scope.month);
 
-            scope.next = function() {
-                var next = scope.month.clone();
-                _removeTime(next.month(next.month()+1).date(1));
-                scope.month.month(scope.month.month()+1);
-                _buildMonth(scope, next, scope.month);
-            };
+      scope.select = function(day) {
+        scope.selected = day.date;
+        console.log(day);
+        //day.date.event another object inside of date
+        //scope.picked
+        //scope.lastDay
+        //scope.ovulation = lastDay - 14
+      };
 
-            scope.previous = function() {
-                var previous = scope.month.clone();
-                _removeTime(previous.month(previous.month()-1).date(1));
-                scope.month.month(scope.month.month()-1);
-                _buildMonth(scope, previous, scope.month);
-            };
-        }
-    };
+      scope.next = function() {
+        var next = scope.month.clone();
+        _removeTime(next.month(next.month() + 1).date(1));
+        scope.month.month(scope.month.month() + 1);
+        _buildMonth(scope, next, scope.month);
+      };
 
-    function _removeTime(date) {
-        return date.day(0).hour(0).minute(0).second(0).millisecond(0);
+      scope.previous = function() {
+        var previous = scope.month.clone();
+        _removeTime(previous.month(previous.month() - 1).date(1));
+        scope.month.month(scope.month.month() - 1);
+        _buildMonth(scope, previous, scope.month);
+      };
     }
+  };
 
-    function _buildMonth(scope, start, month) {
-        scope.weeks = [];
-        var done = false, date = start.clone(), monthIndex = date.month(), count = 0;
-        while (!done) {
-            scope.weeks.push({ days: _buildWeek(date.clone(), month) });
-            date.add(1, "w");
-            done = count++ > 2 && monthIndex !== date.month();
-            monthIndex = date.month();
-        }
-    }
 
-    function _buildWeek(date, month) {
-        var days = [];
-        for (var i = 0; i < 7; i++) {
-            days.push({
-                name: date.format("dd").substring(0, 1),
-                number: date.date(),
-                isCurrentMonth: date.month() === month.month(),
-                isToday: date.isSame(new Date(), "day"),
-                date: date
-            });
-            date = date.clone();
-            date.add(1, "d");
-        }
-        return days;
+
+  function _removeTime(date) {
+    return date.day(0).hour(0).minute(0).second(0).millisecond(0);
+  }
+
+  function _buildMonth(scope, start, month) {
+    scope.weeks = [];
+    var done = false,
+      date = start.clone(),
+      monthIndex = date.month(),
+      count = 0;
+    while (!done) {
+      scope.weeks.push({
+        days: _buildWeek(date.clone(), month)
+      });
+      date.add(1, "w");
+      done = count++ > 2 && monthIndex !== date.month();
+      monthIndex = date.month();
     }
+  }
+
+  function _buildWeek(date, month) {
+    var days = [];
+    for (var i = 0; i < 7; i++) {
+      days.push({
+        name: date.format("dd").substring(0, 1),
+        number: date.date(),
+        isCurrentMonth: date.month() === month.month(),
+        isToday: date.isSame(new Date(), "day"),
+        date: date
+      });
+      date = date.clone();
+      date.add(1, "d");
+    }
+    return days;
+  }
+
 });
+//function _buildBloodTime(bloodyDay) {
+//if (daysInCurrMonth < )
+//  }
+//three if statements for length of months; feb, 30day months, leap years
+// take input date, add (input range / 4)
+// do stuff to (input range/ 4)[each division]
+// .get('/stuff/:day', callback) => DO NOT WRITE 28 ENPOINTS FOR THE LOVE OF ALL THAT IS HOLY DO NOT DO IT
+// bloodTime.push(input-range.length)
+//
+
+
+
+
 
 app.controller('DashCtrl', function($scope) {
 
+$scope.startDate = '';
+
+
+
+$scope.startDateChange = function(val) {
+  console.log("on-change", $scope.startDate);
+
+}
 
 
 })
